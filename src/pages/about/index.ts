aec7a93f302ab2b2859647df02f87534d425eeaa
raphael gregg <raphaelgregg@gmail.com>
2024-03-25 20:44:05 -0300

    fix: padding header about pagem

--- src/pages/about/index.ts
+++ src/pages/about/index.ts
@@ -4,8 +4,11 @@ import { Header } from "../../components/header";
 export function About() {
   return `
     ${Header()}
-
-    <p>working...</p>
+    <div class="about-container">
+      <section class="brief-about">
+        <p>working...</p>
+      </section>
+    </div>
 
     ${Footer()}
   `;
